403b2719df5ba17d67a779a3d5355c863dbe82e2
Gautam bairagi <109797346+GautamBairagi@users.noreply.github.com>
2026-01-03 13:31:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,29 +62,34 @@ const API_VERSION = process.env.API_VERSION || 'v1';
 app.use(helmet());
 
 // CORS
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-//   credentials: true
-// }));
-
-
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://crm-update.netlify.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // for Postman
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    process.env.FRONTEND_URL,
+    'http://localhost:5173',
+    'https://crm-update.netlify.app'
+  ],
   credentials: true
 }));
+
+
+
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://crm-update.netlify.app"
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true); // for Postman
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true
+// }));
 
 // Body parser
 app.use(express.json({ limit: '10mb' }));
